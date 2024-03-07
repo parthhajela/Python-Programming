b780a8656758cd1903366ebe04560d4fd34157eb
parthhajela <124779601+parthhajela@users.noreply.github.com>
2024-03-07 12:51:34 +0530

    Created using Colaboratory

---
+++ Practise.py
@@ -0,0 +1,64 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyOEPZ4LZ/atnMZQkPgIZD9y",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/parthhajela/Python-Programming/blob/main/Practise.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 2,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "1S-SCyY3x37L",
+        "outputId": "39cb383e-bd72-45b9-f7c4-c33034f11e25"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "[1, 2, 3, 4, 5]\n"
+          ]
+        }
+      ],
+      "source": [
+        "my_list = [1,2,3,4]\n",
+        "my_list.append(5)\n",
+        "print(my_list)"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [],
+      "metadata": {
+        "id": "EVKA2E3uyK3L"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
